2036d12d1a3c0ce6217aacf54ec03e1e59e54078
osadolortech <brightosadolor8@gmail.com>
2022-06-15 11:49:30 +0100

    updated the enviroment

---
+++ api/models.py
@@ -0,0 +1,12 @@
+from email.policy import default
+from xmlrpc.client import DateTime
+from django.db import models
+from django.forms import BooleanField, DateField
+
+# Create your models here.
+# class Todolist(models.Model):
+#     title = models.CharField(max_length=100)
+#     completed = models.BooleanField(default=False)
+#     date = models.DateTimeField()
+def work():
+    pass
