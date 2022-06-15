13d376803f817cf4303f0a6e69917a4b0d963de8
osadolortech <brightosadolor8@gmail.com>
2022-06-15 12:38:19 +0100

    updated serilizers, urls,views while using viewset i dont have to write (get,post,put,delete)viewset does all of that for us

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Todolist
+from .serializers import Todoserielizers
 
 # Create your views here.
+
+class TodoAPI(viewsets.ModelViewSet):
+    queryset = Todolist.objects.all()
+    serializer_class =Todoserielizers
